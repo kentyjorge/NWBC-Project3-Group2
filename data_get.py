b00bdc56d0ecb3c057fa500da11ca0b644d687b8
kenneth smith <kennethsmith@kenneths-MacBook-Pro.local>
2022-04-16 11:04:08 -0500

    file updates

--- data_get.py
+++ data_get.py
@@ -9,9 +9,16 @@ from pprint import pprint
 engine = sa.create_engine('postgresql://localhost:5432/group_2_project_3')
 connection = engine.connect()
 
-parks_df = pd.read_sql_table('parks', engine)
-parks_dict = parks_df.to_dict('records')
-pprint(parks_dict)
+#retrieve the park and webcam data from the database
+def getData():
+
+    parks_df = pd.read_sql_table('parks', engine)
+    parks_dict = parks_df.to_dict('records')
+
+    webcams_df = pd.read_sql_table('webcams', engine)
+    webcams_dict = webcams_df.to_dict('records')
+
+    pprint(parks_dict)
 
 
-# return(parks_dict, webcams_dict)
+    return(parks_dict, webcams_dict)
